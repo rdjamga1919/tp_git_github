a280c5591d072fe85e27205ee51d95ae8db3d77b
Anais KRIEGEL--GRAPAIN <KRIEGEL--GRAPAIN_An@lprs.local>
2025-12-01 14:16:41 +0100

    update(): rajout de ligne de code pour demander l'identifiant et le mot de passe a l'utilisateur pour se connecter

--- src/bdd/connexion.php
+++ src/bdd/connexion.php
@@ -1,6 +1,18 @@
 <?php
 try{
 $bdd = new PDO('mysql:host=localhost;dbname=joe', 'joe',  'lovebooks');
-}catch(PDOException $e){
-    echo"Erreur:".$e->getMessage();
+}catch(PDOException $e) {
+    echo "Erreur:" . $e->getMessage();
+}
+// pour recupérer les infos de l'utilisateur
+$log = $pdo->prepare("SELECT * FROM inscrit WHERE identifiant = :identifiant");
+$log->execute(['identifiant' => $identifiant]);
+$user = $log->fetch();
+
+if ($user && password_verify($mot_de_passe, $user['mot_de_passe'])) {
+    // pour si la connexion est resussi réussie
+    $_SESSION['user_id'] = $user['id'];
+    echo "Bienvenue " . $user['identifiant'];
+} else {
+    echo "Identifiant ou mot de passe incorrect.";
 }

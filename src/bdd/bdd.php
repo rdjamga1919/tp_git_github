a6935c2b6cd89ef4bb867b9117310830deb59436
Anais KRIEGEL--GRAPAIN <KRIEGEL--GRAPAIN_An@lprs.local>
2025-12-08 15:28:31 +0100

    update(): continuation du projet

--- src/bdd/bdd.php
+++ src/bdd/bdd.php
@@ -1,7 +1,35 @@
 <?php
-try{
+try {
     $pdo = new PDO('mysql:host=localhost;dbname=joe', 'joe', 'lovebooks');
-}catch(PDOException $e){
-    $error = $e->getMessage();
-    echo $error;
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nom       = $_POST['nom'];
+    $prenom    = $_POST['prenom'];
+    $email     = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $adresse   = $_POST['adresse'] ;
+    $cp        = $_POST['cp'] ;
+    $rue       = $_POST['rue'] ;
+    $ville     = $_POST['ville'] ;
+
+    $stmt = $pdo->prepare("INSERT INTO inscrit (nom, prenom, email, telephone, adresse, cp, rue, ville) 
+        VALUES (:nom, :prenom, :email, :telephone, :adresse, :cp, :rue, :ville)");
+
+    $stmt->execute([
+        'nom'       => $nom,
+        'prenom'    => $prenom,
+        'email'     => $email,
+        'telephone' => $telephone,
+        'adresse'   => $adresse,
+        'cp'        => $cp,
+        'rue'       => $rue,
+        'ville'     => $ville
+    ]);
+
+    header("Location: ../public/index.php");
+    exit();
 }
